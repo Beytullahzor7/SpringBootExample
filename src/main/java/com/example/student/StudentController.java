1fe1630c3eecf87c453a7e8e094656d65d655bb4
Beytullahzor7 <beytullahzor7@gmail.com>
2022-03-11 18:58:16 +0300

    Updated

--- src/main/java/com/example/student/StudentController.java
+++ src/main/java/com/example/student/StudentController.java
@@ -25,4 +25,19 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
     }
+
+    // http://localhost:8080/api/v1/student/2
+   @DeleteMapping(path ="{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    // http://localhost:8080/api/v1/student/2?name=alex&email=alex@gmail.com
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
+    }
 }
